74d513744e0a9b09a87622e68c56e6791cb742c3
Wagner <cartowagner@gmail.com>
2023-05-24 19:51:03 -0500

    Cambios 1

--- src/app/components/contactos/contactos.component.ts
+++ src/app/components/contactos/contactos.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactos.component.css']
 })
 export class ContactosComponent {
+  nombre: string = '';
+  email: string = '';
+  mensaje: string = '';
 
+  enviarFormulario() {
+    // Validar el formulario antes de enviarlo
+    if (this.nombre.trim() === '' || this.email.trim() === '' || this.mensaje.trim() === '') {
+      alert('Por favor, complete todos los campos del formulario.');
+      return;
+    }
+
+    // Aquí puedes agregar la lógica para enviar el formulario
+    console.log('Formulario enviado');
+    console.log('Nombre:', this.nombre);
+    console.log('Email:', this.email);
+    console.log('Mensaje:', this.mensaje);
+
+    // Luego puedes restablecer los campos del formulario
+    this.nombre = '';
+    this.email = '';
+    this.mensaje = '';
+  }
 }
